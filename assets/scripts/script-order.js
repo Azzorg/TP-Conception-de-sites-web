4edc0b9f6ef8c01ed8ccac0ee4b256085ffd3448
Azzorg <c.goupil72@gmail.com>
2017-10-26 02:21:10 -0400

    Order page : en cours, sauvegarde des username & nbCommande

--- assets/scripts/script-order.js
+++ assets/scripts/script-order.js
@@ -1,7 +1,5 @@
-
 $(document).ready(function() {
-
-  $("#order-form").validate({
+  var validator = $("#order-form").validate({
     rules : {
       firstname: {
         required : true,
@@ -29,15 +27,43 @@ $(document).ready(function() {
       },
     },
     messages : {
-        firstname : {
-          required : "test";
-        }
+      firstname : {
+        required : "test",
+        firstname : "Your name has to be in that format"
+      }
     }
   });
 
   $.validator.addMethod("creditcardexpiry", function(value, element) {
-  // allow any non-whitespace characters as the host part
-  return this.optional( element ) || /^(0[1-9]|1[012])\-([1-9]{2})/.test( value );
-  }, 'La date d’expiration de votre carte de crédit est invalide. »');
+    // allow any non-whitespace characters as the host part
+    return this.optional( element ) || /^(0[1-9]|1[012])\/([0-9][0-9])/.test( value );
+    }, 'La date d’expiration de votre carte de crédit est invalide.');
 
+  validator.form();
+
+  //Enregistre 
+  function onClickValidate(){
+    console.log("onClickValidate");
+    var nbCommande = JSON.parse(localStorage.getItem(-1));
+
+    localStorage.clear();
+
+    //Sauvegarde du nom et du prénom de l'utilisateur en localStorage
+    localStorage.setItem(-2, JSON.stringify({firstname : $("#first-name").val(), lastname : $("#last-name".val())}));
+
+    //Sauvegarde du numéro de commande dans le localStorage
+    var nbCommande = JSON.parse(localStorage.getItem(-1));
+    if(nbCommande != null){
+      nbCommande ++;
+      localStorage.setItem(-1, JSON.stringify(nbCommande));
+    }
+    else{
+      nbCommande = 1
+      localStorage.setItem(-1, JSON.stringify(nbCommande));
+    }
+
+
+
+    console.log("nbCommande : " + nbCommande);
+  }
 });
